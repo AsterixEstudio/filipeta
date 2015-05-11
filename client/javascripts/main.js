d185e11ebb3e610aa85c7213d277a56ca4c4300f
leonardo <leonardocamelo.nave@gmail.com>
2015-05-10 23:40:56 -0300

    createCards

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -1,10 +1,21 @@
 Meteor.subscribe('cards');
 
+Template.cards.helpers({
+  cards: function(){
+    return Cards.find({}, {sort:{createdAt: -1}});
+  }
+});
+
 Template.newCard.events({
   'submit form': function(e){
     e.preventDefault();
     var title = e.target.title.value;
     var text = e.target.text.value;
+    e.target.reset();
     Meteor.call('createCard', title, text);
   }
+});
+
+Template.registerHelper('formatDate', function(date){
+  return moment(date).format('DD/MM/YYYY');
 });
